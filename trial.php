d755133bf233fbcbd9384de506d564cf2c3b1461
Gabriel Herrera-Lim <gherrer2@nd.edu>
2020-06-15 22:25:59 -0400

    trial php2

--- trial.php
+++ trial.php
@@ -1,8 +1,31 @@
-<html>
-<body>
 <?php
-    echo $_POST["name"];
-    echo $_POST["email"];
+
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
+
+    if(isset($_POST['submit']))
+    {
+        $UserName = $_POST['sendName'];
+        $Email = $_POST['sendMail'];
+        $Subject = $_POST['sendSubject'];
+        $Message = $_POST['sendMessage'];
+
+        if(empty($UserName) | empty($Email) | empty($Subject) | empty($Message))
+        {
+            header('location: trial-form.html?error')
+        }
+        else{
+            $to = "contactme@gcherreralim.com";
+
+            if(mail($to,$Subject,$Message,$Email))
+            {
+                header("location: trial-form.html?success");
+            }
+        }
+    }
+    else
+    {
+        header("location: trial-form.html");
+    }
 ?>
-<body>
-<html>
