9caf3d1d1964259d7a77ede5b812835dfd3df77a
Gabriel Herrera-Lim <gherrer2@nd.edu>
2020-06-15 23:20:50 -0400

    finalize mailform

--- contactform.php
+++ contactform.php
@@ -1,47 +1,15 @@
-// <?php
-//     ini_set( 'display_errors', 1 );
-//     error_reporting( E_ALL );
-// //     $name = $_GET['name'];
-// //     $subject = $_GET['subject'];
-// //     $mailFrom = $_GET['mail'];
-// //     $message = $_GET['message'];
-//     $name = "Gabby";
-//     $subject = "Hello";
-//     $mailFrom = "gcherreralim@gmail.com";
-//     $message = "Hello";
-//     $to = "contactme@gcherreralim.com";
-//     $headers = "From:" . $mailFrom;
-//     mail($to,$subject,$message, $headers);
-//
-
 <?php
 
-    error_reporting(-1);
-    ini_set('display_errors', 'On');
-    set_error_handler("var_dump");
+    ini_set('display_errors',1);
 
-    if(isset($_POST['submit']))
-    {
-        $UserName = $_POST['sendName'];
-        $Email = $_POST['sendMail'];
-        $Subject = $_POST['sendSubject'];
-        $Message = $_POST['sendMessage'];
+    $name = $_POST["sendName"];
+    $mail = $_POST["sendMail"];
+    $subject = $_POST["sendSubject"];
+    $msg = $_POST["sendMessage"];
+    $to = "contactme@gcherreralim.com";
 
-        if(empty($UserName) | empty($Email) | empty($Subject) | empty($Message))
-        {
-            header('location: index.html?error')
-        }
-        else{
-            $to = "contactme@gcherreralim.com";
+    $messageFull = "Message from: " . $mail . "\n\n" . $msg;
 
-            if(mail($to,$Subject,$Message,$Email))
-            {
-                header("location:index.html?success");
-            }
-        }
-    }
-    else
-    {
-        header("location: index.html");
-    }
+    mail($to, $subject, $messageFull);
+    header("Location: ./#contact-me?mailsend");
 ?>
