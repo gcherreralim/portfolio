abc590cf200463ab2eadff9e59186506148c6eec
Gabriel Herrera-Lim <gherrer2@nd.edu>
2020-06-15 22:45:51 -0400

    trial php5

--- trial.php
+++ trial.php
@@ -1,11 +1,17 @@
 <?php
+
+    ini_set('display_errors',1);
+
     $name = $_POST["sendName"];
     $mail = $_POST["sendMail"];
     $subject = $_POST["sendSubject"];
     $msg = $_POST["sendMessage"];
+    $to = "contactme@gcherreralim.com";
 
-    echo $name;
-    echo $mail;
-    echo $subject;
-    echo $msg;
+    $mail = mail($to, $subject, $msg);
+    if($mail){
+        echo "Thanks!";
+    }else{
+        echo "Oops";
+    }
 ?>
