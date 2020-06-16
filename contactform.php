bd0d314c7d5ba2564582f76e0aa081995bc9765a
Gabriel Herrera-Lim <gherrer2@nd.edu>
2020-06-15 21:04:09 -0400

    trial php

--- contactform.php
+++ contactform.php
@@ -1,11 +1,14 @@
 <?php
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $name = $_GET['name'];
-    $subject = $_GET['subject'];
-    $mailFrom = $_GET['mail'];
-    $message = $_GET['message'];
-    $to = "contactme@gcherreralim.com"
+//     $name = $_GET['name'];
+//     $subject = $_GET['subject'];
+//     $mailFrom = $_GET['mail'];
+//     $message = $_GET['message'];
+    $name = "Gabby";
+    $subject = "Hello";
+    $mailFrom = "gcherreralim@gmail.com";
+    $to = "contactme@gcherreralim.com";
     $headers = "From:" . $mailFrom;
     mail($to,$subject,$message, $headers);
     echo "The email message was sent.";
